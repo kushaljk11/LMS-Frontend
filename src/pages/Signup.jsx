9cb58fbb10bee7e93a767e924797ebf1a5fa05e1
kushaljk11 <np05cp4a240142@iic.edu.np>
2025-09-03 10:29:57 +0545

    fianl

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
     e.preventDefault();
     setLoading(true);
     try {
-      await axios.post("http://localhost:5000/api/auth/register", formData);
+      await axios.post("https://lms-ozcq.onrender.com/api/auth/register", formData);
       toast.success("User registered successfully! Redirecting to login...");
       setTimeout(() => navigate("/login"), 2000); // redirect after 2s
     } catch (error) {
